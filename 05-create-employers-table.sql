ee50c2f65bf2540ee0b729ab1ab993ac2a294182
Rohan Ravi (SX/EMS3) <Ravi.Rohan@bosch.com>
2022-05-01 18:03:49 +0530

    Format and Insert Data

--- 05-create-employers-table.sql
+++ 05-create-employers-table.sql
@@ -2,6 +2,6 @@ CREATE TABLE employers (
     company_name VARCHAR(100),
     company_address VARCHAR(200),
     -- yearly_revenue FLOAT(5,2) -- atmost 5 digits in total and atmost 2 digits after decimal
-    yearly_revenue NUMERIC(5,2), -- () params mandatory but for FLOAT () --> Optional
-    is_hiring BOOLEAN
+    yearly_revenue NUMERIC(5, 2),
+    - - () params mandatory but for FLOAT () -->Optional is_hiring BOOLEAN
 );
---
+++ 07-insert-data.sql
@@ -0,0 +1,15 @@
+-- INSERT INTO employers (company_name,company_address,yearly_revenue,is_hiring)
+-- VALUES ('Bosch', 'KOR, 23', 100.23, TRUE);
+-- INSERT INTO employers (company_name,company_address,yearly_revenue,is_hiring)
+-- VALUES ('RBEI', 'ADU, 231', 10.23, FALSE);
+-- INSERT INTO employers (company_name,company_address,yearly_revenue,is_hiring)
+-- VALUES ('RBIN', 'OMTP, 234', 0.23, TRUE);
+-- INSERT INTO employers (company_name,company_address,yearly_revenue,is_hiring)
+-- VALUES ('BGSW', 'BEL, 232', 8.23, FALSE);
+INSERT INTO conversations (user_name, employer_name, message, date_sent)
+VALUES (
+        'ROHAN',
+        'Bosch',
+        'Hello!! I am interested',
+        '2022-05-04 18:02:32'
+    );
