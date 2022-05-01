c29e1c654ca91632b236b456fa9e712a8652fa1c
Rohan Ravi (SX/EMS3) <Ravi.Rohan@bosch.com>
2022-05-01 17:47:19 +0530

    New Tables

---
+++ 05-create-employers-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employers (
+    company_name VARCHAR(100),
+    company_address VARCHAR(200),
+    -- yearly_revenue FLOAT(5,2) -- atmost 5 digits in total and atmost 2 digits after decimal
+    yearly_revenue NUMERIC(5,2), -- () params mandatory but for FLOAT () --> Optional
+    is_hiring BOOLEAN
+);
---
+++ 06-create-conversations-table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE conversations (
+    user_name VARCHAR(100),
+    employer_name VARCHAR(100),
+    message TEXT,
+    date_sent TIMESTAMP
+);
