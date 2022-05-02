cbc612bf0449e6f896f4f6a83c25544723b698e5
Rohan Ravi (SX/EMS3) <Ravi.Rohan@bosch.com>
2022-05-02 15:26:56 +0530

    Update VARCHAR column

--- 10-update-table.sql
+++ 10-update-table.sql
@@ -1,7 +1,12 @@
 ALTER TABLE employers
 MODIFY COLUMN yearly_revenue FLOAT;
+ALTER TABLE users
+MODIFY COLUMN full_name VARCHAR(300);
 --MySQL
 ALTER TABLE employers
 ALTER COLUMN yearly_revenue
 SET DATA TYPE FLOAT;
+ALTER TABLE users
+ALTER COLUMN full_name
+SET DATA TYPE VARCHAR(300);
 --postgreSQL
