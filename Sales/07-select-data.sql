4c387f72a649aefa5bf2ff39a72b5bd642a9dcbc
Rohan Ravi (SX/EMS3) <Ravi.Rohan@bosch.com>
2022-05-12 21:11:57 +0530

    SELECT data

---
+++ Sales/07-select-data.sql
@@ -0,0 +1,10 @@
+-- SELECT *
+-- FROM sales;
+-- SELECT id,
+--     product_name,
+--     customer_name
+-- from sales;
+SELECT id AS num,
+    customer_name AS C_name,
+    product_name as p_name
+FROM sales;
